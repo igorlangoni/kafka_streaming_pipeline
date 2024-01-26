a3295344c39813b36dbbb818efc4ffdd1fc90a27
Igor Langoni <123383171+igorlangoni@users.noreply.github.com>
2024-01-26 15:19:31 +0000

    created format_data task

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -2,28 +2,48 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+
 default_args = {
     'owner': 'airscholar',
     'start_date': datetime(2024, 1, 1, 10, 00),
 }
 
+
 def get_data():
     import requests
+    import json
 
     res = requests.get('https://randomuser.me/api/')
     res = res.json()
     res = res['results'][0]
 
-    return res
+    # print(json.dumps(res, indent=3))
+
 
 def format_data(res):
     data = {}
-    
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']} " \
+                    f"{location['city']}, {location['state']}, {location['country']}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
 
 def stream_data():
     import json
-    # print(json.dump(res, indent=3)) --- formats the json nicely
-
+    res = get_data()
+    res = format_data(res)
+    print(res)
 
 
 with DAG(
@@ -33,9 +53,11 @@ with DAG(
     catchup=False
 ) as dag:
 
+
     streaming_task = PythonOperator(
         task_id='stream_data_from_api',
         python_callable=stream_data
     )
+
 
 stream_data()
